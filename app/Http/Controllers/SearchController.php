ea41193ca7797f7769a0d851ef95c2f546896fae
Gregoire Meylan <gmeylan@gmail.com>
2016-03-29 13:49:59 +0200

    Ajout de la vue pour la recherche

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use DB;
+use View;
 
 class SearchController extends Controller
 {
@@ -21,11 +22,18 @@ class SearchController extends Controller
                 $s .= ' ';
             $c++;
         }
-
         $texts = DB::table('texts')
             ->whereRaw(DB::raw("MATCH(title, body) AGAINST('".$s."' IN BOOLEAN MODE)"))->get();
+        $results = [];
+        $i = 0;
 
-
-        return $texts;
+        foreach($texts as $text){
+            $array['body'] = $text->body;
+            $array['title'] = $text->title;
+           $results[$i]['data'] = $array;
+           $i++;
+       }
+        $resultsEncoded = json_encode($results);
+        return View::make('search')->with('results',$resultsEncoded);
     }
 }
---
+++ resources/views/search.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        @include('layouts.head')
+    </head>
+    <body>
+    <h1>Résultat</h1>
+
+        {{ $results }}
+
+    </body>
+</html>
